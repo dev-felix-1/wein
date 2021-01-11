5909fd96c218816203c716b380a6529f08e17aa4
kle14503 <felix.kleine-wilde@msg-life.com>
2021-01-11 14:51:13 +0000

    split

---
+++ wein/src/main/java/de/fekl/dine/api/core/NodeRoles.java
@@ -0,0 +1,14 @@
+package de.fekl.dine.api.core;
+
+@Deprecated
+public final class NodeRoles {
+
+	private NodeRoles() {
+
+	}
+
+	public static final String START = "START";
+	public static final String END = "END";
+	public static final String INTERMEDIATE = "INTERMEDIATE";
+
+}
